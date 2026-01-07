140c8974f26cff32501ca9396af9d2d653adefb0
israekor <korchi.israe@etu.uae.ac.ma>
2026-01-07 11:55:08 +0100

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
 from app.main import app
 
-def test_hello_world():
-  client = app.test_client()
-  response = client.get('/')
 
-  assert response.status_code == 200
-  assert b'Hello World!' in response.data
+def test_hello_world():
+    client = app.test_client()
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert b'Hello World!' in response.data
